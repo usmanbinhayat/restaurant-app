3e08491ac9cecaeac4fff61630c99c541b6621b5
usmanbinhayat <usmanbinhayat6996@gmail.com>
2026-09-02 06:52:55 +0500

    ! removed

--- app/kitchen/page.js
+++ app/kitchen/page.js
@@ -4,8 +4,8 @@ import { useEffect, useState } from 'react'
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
 
 export default function KitchenPage() {
